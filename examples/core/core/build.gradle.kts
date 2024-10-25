0ea5420017612b86fe80750172c427a9ebe6ef77
Natan <xpenatan@gmail.com>
2024-10-25 20:34:46 -0300

    Remove implementation

--- examples/core/core/build.gradle.kts
+++ examples/core/core/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
     implementation("com.badlogicgames.gdx:gdx:${LibExt.gdxVersion}")
-    implementation("com.badlogicgames.gdx:gdx-box2d:${LibExt.gdxVersion}")
-    implementation("com.badlogicgames.gdx:gdx-freetype:${LibExt.gdxVersion}")
     implementation("com.badlogicgames.gdx:gdx-ai:${LibExt.aiVersion}")
 
     implementation("com.github.mgsx-dev.gdx-gltf:gltf:${LibExt.gdxGltfVersion}")
